8f774d923d581621b49e5d312d5f88653a9d21a6
Fikri Firdaus <fikri.firdaus26@gmail.com>
2019-10-02 10:47:04 +0700

    better

--- src/components/Footers/SimpleFooter.js
+++ src/components/Footers/SimpleFooter.js
@@ -21,27 +21,27 @@ class SimpleFooter extends React.Component {
             <Row className=" row-grid align-items-center mb-5">
               <Col lg="6">
                 <h3 className=" text-primary font-weight-light mb-2">
-                  Terhubung dengan kami
+                  Let's get in touch
                 </h3>
-                <h4 className=" mb-0 font-weight-light">
-                  Ketahui Lebih Jauh Tentang Qlola
-                </h4>
+                {/* <h4 className=" mb-0 font-weight-light">
+                  Let's get in touch
+                </h4> */}
               </Col>
               <Col className=" text-lg-center btn-wrapper" lg="6">
                 <Button
                   className=" btn-neutral btn-icon-only btn-round"
-                  color="twitter"
-                  href="https://twitter.com/creativetim"
+                  color="instagram"
+                  href="https://www.instagram.com/qlolaworks"
                   id="tooltip126536702"
                   size="lg"
                   target="_blank"
                 >
-                  <i className=" fa fa-twitter" />
+                  <i className=" fa fa-instagram" />
                 </Button>
                 <UncontrolledTooltip delay={0} target="tooltip126536702">
                   Follow us
                 </UncontrolledTooltip>
-                <Button
+                {/* <Button
                   className=" btn-neutral btn-icon-only btn-round ml-1"
                   color="facebook"
                   href="https://www.facebook.com/creativetim"
@@ -66,7 +66,7 @@ class SimpleFooter extends React.Component {
                 </Button>
                 <UncontrolledTooltip delay={0} target="tooltip626177562">
                   Watch us
-                </UncontrolledTooltip>
+                </UncontrolledTooltip> */}
               </Col>
             </Row>
             <hr />
@@ -75,7 +75,7 @@ class SimpleFooter extends React.Component {
                 <div className=" copyright">
                   © {new Date().getFullYear()}{" "}
                   <a
-                    href="https://www.creative-tim.com?ref=adsr-footer"
+                    href="/"
                     target="_blank"
                   >
                     Qlola
